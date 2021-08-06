afecca730a5ff21f06635ab30da9c9aa3808c614
joseporres <joseporres@users.noreply.github.com>
2021-08-06 14:28:26 -0500

    knnrtree finished

--- busquedas.py
+++ busquedas.py
@@ -49,6 +49,17 @@ def rangeSearch(r, Q, n):
     lim.extend( [ i + r for i in Q ])
     return [x.object for x in Rtree.intersection(lim, objects=True)]
 
+def knnRtree(k, Q, n):
+  p = index.Property()
+  p.dimension = 128 
+  p.buffering_capacity = 10 
+ 
+  Rtree = index.Rtree("preprocess/Rtree"+ str(n), properties=p)  
+  listQ = list(Q)
+  for i in Q:
+    listQ.append(i)
+
+  return list(Rtree.nearest(coordinates=listQ, num_results=k, objects='raw'))
 
 # print(rangeSearch(0.2
 # , face_recognition.face_encodings(face_recognition.load_image_file('fotos_prueba/unknown.jpg'))[0]
