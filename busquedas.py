cca99dcf654d036894c6821dfdec3c85c6ae5a83
DiegoOrtizS <52261652+DiegoOrtizS@users.noreply.github.com>
2021-08-06 14:25:28 -0500

    range search y knn sequential

---
+++ busquedas.py
@@ -0,0 +1,64 @@
+import face_recognition
+from rtree import index 
+import os
+import heapq
+import json
+import linecache
+
+def knnSequential(k, Q, n):  
+    cont = 0  
+    ruta = []
+    caras = []
+
+    for pathF in os.listdir("preprocess/lfw"):
+        auxPath = "preprocess/lfw/" + pathF
+        images = os.listdir(auxPath)
+
+        for nameF in images: 
+            auxPath2 = auxPath + "/" + nameF
+            for it in face_recognition.face_encodings(face_recognition.load_image_file(auxPath2)):
+                if cont == n:
+                    dist = face_recognition.face_distance(caras, Q)
+                    auxArr = []
+                    for i in range(0, cont, 1):
+                        auxArr.append((dist[i], ruta[i]))
+                    heapq.heapify(auxArr)    
+                    return heapq.nsmallest(k, auxArr)
+
+                ruta.append(auxPath2)
+                caras.append(it)        
+                cont += 1
+
+
+    dist = face_recognition.face_distance(caras, Q)
+    auxArr = [] 
+
+    for i in range(0, cont, 1):
+        auxArr.append((dist[i], ruta[i]))
+    heapq.heapify(auxArr)    
+    return heapq.nsmallest(k, auxArr)
+
+
+def rangeSearch(r, Q, n):
+    p = index.Property()
+    p.dimension = 128
+    p.buffering_capacity = 3
+    Rtree = index.Rtree("preprocess/Rtree"+ str(n), properties=p)  
+    lim = []
+    lim.extend( [ i - r for i in Q ])
+    lim.extend( [ i + r for i in Q ])
+    return [x.object for x in Rtree.intersection(lim, objects=True)]
+
+
+# print(rangeSearch(0.2
+# , face_recognition.face_encodings(face_recognition.load_image_file('fotos_prueba/unknown.jpg'))[0]
+# , 100))
+
+# print(knnRtree(5
+# , face_recognition.face_encodings(face_recognition.load_image_file('fotos_prueba/unknown.jpg'))[0]
+# , 100))
+
+# print(knnSequential(5
+# , face_recognition.face_encodings(face_recognition.load_image_file('fotos_prueba/unknown.jpg'))[0]
+# , 100))
+
